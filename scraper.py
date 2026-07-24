e1f709a7f8505eb71be5ff2c0792ff858c6d0f2a
Ashfatul <ashfatul.islam@gmail.com>
2026-07-24 17:18:41 +0600

    Add browser headers to scraper

--- scraper.py
+++ scraper.py
@@ -7,6 +7,11 @@ class NescoScraper:
     def __init__(self):
         self.url = "https://customer.nesco.gov.bd/pre/panel"
         self.session = requests.Session()
+        self.session.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.9',
+        })
         import urllib3
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         
